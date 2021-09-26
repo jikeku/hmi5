7a76e91e0e928b175398fc7a2b90aac312fa3e73
jikeku <48707088+jikeku@users.noreply.github.com>
2021-09-27 01:08:05 +0800

    Update dlink.lua

--- trunk/user/shadowsocks/ss/dlink.lua
+++ trunk/user/shadowsocks/ss/dlink.lua
@@ -257,11 +257,10 @@ local function processData(szType, content)
 				result.tls = "2"
 				result.tls_host = params.host
 				result.insecure = 0
-				if params.flow == "xtls-rprx-splice" then
+				if params.flow == "rprx-xtls-splice" then
 					result.flow = "2"
 				else
 					result.flow = "1"
-				end	
 			else
 				result.tls = "0"
 			end
